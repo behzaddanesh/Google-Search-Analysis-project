3885b6b170e3b45faa71046bbd49fef4cbefd1e1
Behzad <126592738+behzaddanesh@users.noreply.github.com>
2023-04-28 14:55:07 +0200

    Update Google Search Analysis project.py

--- Google Search Analysis project.py
+++ Google Search Analysis project.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pytrends.request import TrendReq
 trends = TrendReq()
+#If the code has an error, go to the terminal and install pandas and pytrends 
 
 trends.build_payload(kw_list = ["Woman Life Freedom"])
 dt = trends.interest_by_region()
